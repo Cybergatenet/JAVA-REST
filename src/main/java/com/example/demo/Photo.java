024bb657a531409bda6cb782b8cdba9cd28502e9
Cybergatenet <abelchinedu2@gmail.com>
2025-02-14 06:38:05 -0800

    added the collection of photo class

--- src/main/java/com/example/demo/Photo.java
+++ src/main/java/com/example/demo/Photo.java
@@ -1,6 +1,19 @@
 package com.example.demo;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Photo {
    private int Id = 1;
    private String username = "Cybergate";
+
+   private Map<String, Photo> db = new HashMap<>() {{
+      put("1", new Photo());
+   }};
+
+   public Collection<Photo> get() {
+      return null;
+   }
+   
 }
