871eab703ce540901b542d26619ea3d47f2b1890
Cybergatenet <abelchinedu2@gmail.com>
2025-02-18 08:08:51 -0800

    added some anotations

--- src/main/java/com/example/demo/Photo.java
+++ src/main/java/com/example/demo/Photo.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
 public class Photo {
    private int Id = 1;
    private String username = "Cybergate";
@@ -12,7 +16,7 @@ public class Photo {
       db.put("1", new Photo());
    }};
 
-   public Collection<Photo> getPhotos() {
+   public Collection<Photo> getPhotos(@PathVariable @Validated MultipartFile data) {
       return null;
    }
    
